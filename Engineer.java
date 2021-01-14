47a228e892a14d7bdfba1b75b1b1be139192bf42
Haizhao-Li <77393330+Haizhao-Li@users.noreply.github.com>
2021-01-14 21:26:01 +0700

    Delete Engineer.java

--- Engineer.java
+++
@@ -1,7 +0,0 @@
-public class Engineer extends Person{
-    public Engineer(String name, int age,String gender){
-        this.name = name;
-        this.age = age;
-        this.gender = gender;
-    }
-}
