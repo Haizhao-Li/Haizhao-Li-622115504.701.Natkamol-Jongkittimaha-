e32736a4bc64a91958b75309ffae20161d1f80a6
Haizhao-Li <77393330+Haizhao-Li@users.noreply.github.com>
2021-01-14 21:25:48 +0700

    Delete Designer.java

--- Designer.java
+++
@@ -1,8 +0,0 @@
-public class Designer extends Person{
-    public Designer(String name, int age,String gender){
-        this.name = name;
-        this.age = age;
-        this.gender = gender;
-    }
-}
-
