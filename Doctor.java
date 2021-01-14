780d8b332ed11eabbf7db7fd80a06c27686e3a05
Haizhao-Li <77393330+Haizhao-Li@users.noreply.github.com>
2021-01-14 21:25:55 +0700

    Delete Doctor.java

--- Doctor.java
+++
@@ -1,8 +0,0 @@
-public class Doctor extends Person{
-
-    public Doctor(String name, int age,String gender){
-        this.name = name;
-        this.age = age;
-        this.gender = gender;
-    }
-}
