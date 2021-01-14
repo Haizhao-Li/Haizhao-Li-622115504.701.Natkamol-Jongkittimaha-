03b3bcc2f5bbbf28a7950a1f3a8d497908768c21
Haizhao-Li <77393330+Haizhao-Li@users.noreply.github.com>
2021-01-14 21:26:22 +0700

    Delete iheritance.java

--- iheritance.java
+++
@@ -1,8 +0,0 @@
-public class iheritance {
-    public static void main(String[] args) {
-        Artist art = new Artist("Ball",20,"Male");
-        art.genre = "hip hop";
-        art.introduce();
-        art.playMusic();
-    }
-}
