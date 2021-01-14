e4aeadc1609030a27121a28e227e937320fa953a
Haizhao-Li <77393330+Haizhao-Li@users.noreply.github.com>
2021-01-14 21:26:28 +0700

    Delete Teacher.java

--- Teacher.java
+++
@@ -1,9 +0,0 @@
-public class Teacher extends Person {
-
-    public Teacher(String name, int age,String gender){
-        this.name = name;
-        this.age = age;
-        this.gender = gender;
-    }
-
-}
