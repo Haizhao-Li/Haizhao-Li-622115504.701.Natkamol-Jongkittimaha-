91932e541d1186538c7a6a217c3ddc0f48c81824
Haizhao-Li <77393330+Haizhao-Li@users.noreply.github.com>
2021-01-14 21:26:14 +0700

    Delete Sport.java

--- Sport.java
+++
@@ -1,7 +0,0 @@
-public class Sport extends Person{
-    public Sport(String name, int age,String gender){
-        this.name = name;
-        this.age = age;
-        this.gender = gender;
-    }
-}
